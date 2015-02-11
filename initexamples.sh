d928fa3ee62fe30d6d4c6b525a4cd7359576b7c5
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-02-11 12:22:47 -0700

    adding travis build for uno via leo

---
+++ initexamples.sh
@@ -0,0 +1,23 @@
+#!/bin/bash       
+for d in examples/*/ ; do
+cat <<EOF > $d/package.json
+{
+  "name": "basic",
+  "version": "0.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC"
+}
+EOF
+mkdir $d/node_modules
+mkdir $d/src
+cp $d/*.ino $d/src
+ln -s $PWD $d/node_modules/
+pushd $d
+leo build -b uno
+popd
+done
